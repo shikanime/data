95230c30b410a886a4f8aa057a118975f574486c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-21 18:05:12 +0000

    Add Binance data source
    [ghstack-poisoned]

--- infra/storage.tf
+++ infra/storage.tf
@@ -7,9 +7,11 @@ module "cloud_storage" {
   prefix     = "${var.project}-${var.name}-${lower(var.location)}"
   names = [
     "sg-datalake",
+    "binance-datalake",
   ]
   versioning = {
     "sg-datalake" = true
+    "binance-datalake" = true
   }
   viewers = module.service_accounts.emails_list
 }
